ace1d22ec9669d9682b129ef8084fc934d534f26
Alam Guardin <alamguardin@gmail.com>
2025-02-13 21:21:14 -0500

    chore: get users from db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,47 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const db = await open({
+	filename: path.resolve(__dirname, './db/users.db'),
+	driver: sqlite3.Database,
+});
+
+await db.exec(/*SQL*/ `
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        last_name TEXT,
+        username TEXT UNIQUE,
+        age INTEGER
+    )
+`);
+
+// const listUsers = [
+// 	{ name: 'Jhon', lastName: 'Doe' },
+// 	{ name: 'Jay', lastName: 'Stevents' },
+// 	{ name: 'Carl', lastName: 'Madison' },
+// 	{ name: 'Ferd', lastName: 'MCdonald' },
+// 	{ name: 'Anya', lastName: 'Taylor' },
+// ];
+
+// const stmt = await db.prepare(
+// 	'INSERT INTO users (name, last_name) VALUES (?,?)',
+// );
+
+// for (const user of listUsers) {
+// 	stmt.run(user.name, user.lastName);
+// }
+
+// await stmt.finalize();
+
+// await db.close();
 
 const app = new Hono();
 
@@ -7,11 +49,9 @@ app.get('/', (c) => {
 	return c.text('Hello Hono!');
 });
 
-app.get('/api/hello', (c) => {
-	return c.json({
-		ok: true,
-		message: 'Hello Fiber!',
-	});
+app.get('/api/hello', async (c) => {
+	const users = await db.all('SELECT * FROM users');
+	return c.json(users);
 });
 
 const port = 3000;
